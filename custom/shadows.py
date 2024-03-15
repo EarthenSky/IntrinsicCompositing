bcbd24edd139114717820d8b1edf05de2506b7af
EarthenSky Debian <null>
2024-03-15 10:13:17 -0700

    add support for isolating the generated shadow & fix the raymarching

--- custom/shadows.py
+++ custom/shadows.py
@@ -1,14 +1,16 @@
 import os
 
 import numpy as np
+import skimage.transform
 
 from chrislib.data_util import load_image
 from chrislib.general import invert, uninvert, view, np_to_pil, to2np, add_chan
 
 def calculate_screen_space_shadows(
     light_direction:np.ndarray[np.float64], # [x,y,z]
-    depth_map,
+    depth_map, # closest values will be 1.0, while furthest values will be 0.0
     composite_mask, # NOTE: if this mask is set to always be one, then the mask will display all shadows
+    composite_mask_depth:float,
     depth_multiplier:float=1.0,
 ):
     """
@@ -21,34 +23,40 @@ def calculate_screen_space_shadows(
     - a colored mask representing which pixels were shaded by the new composited image
     """
 
-    MAX_STEPS = 8 #16
+    MAX_STEPS = 32
     height, width = depth_map.shape[0], depth_map.shape[1]
 
     shaded_mask = np.zeros_like(composite_mask)
 
     # a step should be the correct size so that the last step ends up higher than the max depth in the image.
-    max_depth = -depth_multiplier * np.max(depth_map)
-    min_depth = -depth_multiplier * np.min(depth_map)
-    light_direction = light_direction / np.linalg.norm(light_direction)
-    step_vector = light_direction / light_direction[2] * (min_depth - max_depth) / MAX_STEPS # TODO: is this backwards?
+    max_depth = depth_multiplier * np.max(depth_map)
+    min_depth = depth_multiplier * np.min(depth_map)
+    print(f"\tmax_depth:{max_depth}")
+    print(f"\tmin_depth:{min_depth}")
 
-    print(step_vector)
+    light_direction = light_direction / np.linalg.norm(light_direction)
+    step_vector = -light_direction / light_direction[2] * (min_depth - max_depth) / MAX_STEPS
+    print(f"\tstep_vector (in px): {step_vector}")
 
     for y in range(height):
+        if y % 50 == 0: print(f"\ty:{y}")
         
-        if y % 50 == 0: 
-            print(f"\ty:{y}")
-        
-        # TODO: run the following using a compute shader or something
+        # TODO: implement this using raycasting instead of raymarching
+        # Intersect ray with the quad generated from the corners of the pixel 
         for x in range(width):
             z = depth_multiplier * depth_map[y, x]
             camera_relative_coord = np.asarray([x, y, z])
         
             for step in range(MAX_STEPS):
                 camera_relative_coord += step_vector
-                
+
                 approx_x = int(camera_relative_coord[0])
                 approx_y = int(camera_relative_coord[1])
+
+                # DEBUG:
+                #if(x == 500 and y == 500):
+                #    print(f"\t{camera_relative_coord}")
+                #    print(f"\t{depth_map[approx_y, approx_x]}")
 
                 # check boundary conditions
                 if approx_x < 0 or approx_x > depth_map.shape[1]:
@@ -57,10 +65,10 @@ def calculate_screen_space_shadows(
                     break
                 
                 # take a step, and check the texture again
-                if camera_relative_coord[2] < depth_map[approx_y, approx_x]:
-                    # TODO: figure out why the shadows aren't...
+                if camera_relative_coord[2] < depth_multiplier * depth_map[approx_y, approx_x]:
                     # set shadow iff the shadow was being casted by our object specifically
-                    shaded_mask[y, x] = composite_mask[approx_y, approx_x]
+                    shaded_mask[y, x] = 1 if composite_mask[approx_y, approx_x] > 0.0 else 0
+                    
 
     return shaded_mask
 
@@ -68,17 +76,50 @@ if __name__ == "__main__":
     
     COMBINED_DEPTH_MAP_PATH = "./output/cycling-8215973_shampoo-1860642_depth.png"
     COMBINED_NAME = "cycling-8215973_shampoo-1860642" # os.path.basename(COMBINED_DEPTH_MAP_PATH).split(".")[0]
-    #ORIGINAL_DEPTH_MAP = ""
 
+    #ORIGINAL_DEPTH_MAP = ""
+    
+    COMPOSITE_MASK = "./output/shampoo-1860642_mask.png"
+    COMPOSITE_NAME = "shampoo-1860642_mask"
+    COMPOSITE_SCALE = 0.25 # how large the fg image should be when compared to the bg
+    COMPOSITE_LOCATION = [0.55, 0.04]
+    
+    # --------------------------------------------------- #
+    
     print("1. load & generate our images")
     combined_depth_map = load_image(COMBINED_DEPTH_MAP_PATH)
-    everything_mask = np.ones_like(combined_depth_map, dtype=np.uint8)
+
+    # get "full image mask" from the selected area mask 
+    fg_mask_crop = load_image(COMPOSITE_MASK)
+    fg_mask_rescaled = skimage.transform.resize(fg_mask_crop, (
+        fg_mask_crop.shape[0] * COMPOSITE_SCALE, 
+        fg_mask_crop.shape[1] * COMPOSITE_SCALE
+    ), anti_aliasing=True)
+    full_size_mask = np.zeros_like(combined_depth_map, dtype=np.float64)
+    top = int(COMPOSITE_LOCATION[0] * combined_depth_map.shape[0])
+    left = int(COMPOSITE_LOCATION[1] * combined_depth_map.shape[1])
+    full_size_mask[
+        top : top + fg_mask_rescaled.shape[0], 
+        left : left + fg_mask_rescaled.shape[1]
+    ] = fg_mask_rescaled
+
+    # everything_mask = np.ones_like(combined_depth_map, dtype=np.uint8)
 
     print("2. generate shaded maps")
-    light_direction = np.asarray([1, 1, -1])
-    shaded_mask = calculate_screen_space_shadows(light_direction, combined_depth_map, everything_mask, depth_multiplier=0.8)
+    light_direction = np.asarray([16, 16, -1])
+    shaded_mask = calculate_screen_space_shadows(
+        light_direction, 
+        combined_depth_map, 
+        full_size_mask, 
+        8.0, 
+        depth_multiplier=64.0,
+    )
 
     # TODO: xor the screen-space shadows with 
 
     print("4. save output")
+    np_to_pil(full_size_mask).save(f"output/{COMPOSITE_NAME}_full_mask.png")
+    np_to_pil(full_size_mask).save(f"full_mask.png")
+
     np_to_pil(shaded_mask).save(f"output/{COMBINED_NAME}_shaded_mask.png")
+    np_to_pil(shaded_mask).save(f"shaded_mask.png")
